8f2f6156c039c8055c8a04d06013514c6bba92aa
daniloccangucu <daniloccangucu@gmail.com>
2024-01-04 17:17:11 +0100

    removing secret, using custom quality gate

--- backend/media/src/test/java/SimpleTest.java
+++ backend/media/src/test/java/SimpleTest.java
@@ -3,13 +3,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleTest {
-
-    private static final String MY_****"MY_SECRET_KEY";
-
     @Test
     public void alwaysPassingTest() {
         assertTrue(true);
-        System.out.println(MY_SECRET_KEY);
     }
 
     /*
