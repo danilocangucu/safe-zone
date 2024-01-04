83528eaeaa587c89a857c48e81cc1159077b1bbe
daniloccangucu <daniloccangucu@gmail.com>
2024-01-04 16:52:27 +0100

    secret in test

--- backend/media/src/test/java/SimpleTest.java
+++ backend/media/src/test/java/SimpleTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleTest {
 
+    private static final String MY_****"MY_SECRET_KEY";
+
     @Test
     public void alwaysPassingTest() {
         assertTrue(true);
+        System.out.println(MY_SECRET_KEY);
     }
 
     /*
